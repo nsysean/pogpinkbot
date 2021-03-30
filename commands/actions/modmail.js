120fbe7640cd86e4c10ff670a141b4951f8b43d9
nsysean <76026698+nsysean@users.noreply.github.com>
2021-03-30 09:44:31 +0800

    Update modmail.js

--- commands/actions/modmail.js
+++ commands/actions/modmail.js
@@ -16,6 +16,6 @@ module.exports = {
         const person = message.mentions.members.first()
         if(!person){return message.reply("Which staff are you mailing to..?")}
         if(!person.permissions.has("ADMINISTRATOR")){return message.reply("Sorry that member isn't an admin, therefore you cant mod mail them.")}
-person.send(`Mod mail from ${message.author.username}: ${args.slice(1)}`);
+person.send(`Mod mail from ${message.author.username}: ${args.slice(1).join(" "}`);
 message.reply("Mod mail sent")
 }}
